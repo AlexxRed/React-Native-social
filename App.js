63bdf9c4e2a348eaf27bade456f25455901447a0
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2022-08-17 14:15:39 +0300

    add basic

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+      style={styles.image}
+      source={require('./assets/images/hd-wallpaper-g894f88c32_1280.jpg')}>
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.inputTitle}>Email addres</Text>
+          <TextInput style={styles.loginInput} textAlign={'center'}/>
+        </View>
+        <View style={{marginTop: 20}}>
+          <Text style={styles.inputTitle}>Password</Text>
+          <TextInput style={styles.loginInput} textAlign={'center'} secureTextEntry={true}/>
+        </View>
+      </View>
+      </ImageBackground>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +28,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  text:{
+    color: "blue",
+     fontSize: 25
+  },
+  textBox:{
+    borderWidth: 2,
+    borderColor: "tomato",
+    borderRadius: 10,
+    padding: 10
+  },
+  image:{
+    flex:1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  loginInput:{
+    // marginHorizontal: 20,
+    height:40,
+    color: "#fff",
+    borderColor:'#fff',
+    borderRadius: 5,
+    borderWidth: 2
+    
+  },
+  form:{
+    marginHorizontal: 20,
+  },
+  inputTitle:{
+    paddingBottom: 10,
+    color:'#fff',
+    fontSize: 18,
+  }, 
 });
